cda3eefa93b265fdbe71c0399631455d252ad357
Brandon Chin-A-Lien <bchinalien@outlook.com>
2024-04-11 17:57:13 +0200

    removed unnneeded visualizations lexer.ipynb

--- spl_parser.py
+++ spl_parser.py
@@ -2,29 +2,30 @@ import lark
 from lark import Lark
 
 grammar = """
-start:     vardecl
-vardecl:   "var" ID "=" exp ";"
-?exp:       exp OP2 exp_hipr        
-            | exp_hipr
+    start:     vardecl
+    vardecl:   "var" ID "=" exp ";"
+    ?exp:       exp OP2 exp_hipr        
+                | exp_hipr
 
-?exp_hipr:    ID
-            | INT
-            | OP1 exp           
-            | "(" exp ")"       
+    ?exp_hipr:    ID
+                | INT
+                | OP1 exp           
+                | "(" exp ")"       
 
-OP2:        "+" | "-" | "*" | "/" | "%"
-            | "==" | "<" | ">" | "<=" | ">=" | "!="
-            | "&&" | "||"
+    OP2:        "+" | "-" | "*" | "/" | "%"
+                | "==" | "<" | ">" | "<=" | ">=" | "!="
+                | "&&" | "||"
 
-OP1:        "!" | "-"
-INT:        /-?\\d+/
-BOOL:       "False" | "True"
-ID:         /[a-zA-Z_][a-zA-Z0-9_]*/
+    OP1:        "!" | "-"
+    INT:        /-?\\d+/
+    BOOL:       "False" | "True"
+    ID:         /[a-zA-Z_][a-zA-Z0-9_]*/
 
 
-%ignore /\\s/ 
-        | /\\/\\/.*/
+    %ignore /\\s/ 
+            | /\\/\\/.*/
 
 """
 
 spl_parser = Lark(grammar, parser='lalr', debug=True)
+
