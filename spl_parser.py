8d6b2b6c7b5e64dd4c7ca51f97d8ce81b60cacb8
Brandon Chin-A-Lien <bchinalien@outlook.com>
2024-04-11 17:38:04 +0200

    updated to new grammar

---
+++ spl_parser.py
@@ -0,0 +1,30 @@
+import lark
+from lark import Lark
+
+grammar = """
+start:     vardecl
+vardecl:   "var" ID "=" exp ";"
+?exp:       exp OP2 exp_hipr        
+            | exp_hipr
+
+?exp_hipr:    ID
+            | INT
+            | OP1 exp           
+            | "(" exp ")"       
+
+OP2:        "+" | "-" | "*" | "/" | "%"
+            | "==" | "<" | ">" | "<=" | ">=" | "!="
+            | "&&" | "||"
+
+OP1:        "!" | "-"
+INT:        /-?\\d+/
+BOOL:       "False" | "True"
+ID:         /[a-zA-Z_][a-zA-Z0-9_]*/
+
+
+%ignore /\\s/ 
+        | /\\/\\/.*/
+
+"""
+
+spl_parser = Lark(grammar, parser='lalr', debug=True)
